62a881f281b3597cac58a381110cfb610a12d1bd
VladimirSafronov <safron5000@mail.ru>
2022-10-19 20:28:42 +0300

    created rest api chapter1
    * added application.properties
    * copied entity, dao, service, controller from spring_rest

---
+++ src/main/java/com/zaurtregulov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,44 @@
+package com.zaurtregulov.spring.springboot.springboot_rest.dao;
+
+
+import com.zaurtregulov.spring.springboot.springboot_rest.entity.Employee;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+  //создание EntityManager происходит за кулисами (это SessionFactory из Hibernate)
+  @Autowired
+  private EntityManager entityManager;
+
+  public List<Employee> getAllEmployees() {
+    //получение hibernate сессии
+    //EntityManager это обертка Session
+    Session session = entityManager.unwrap(Session.class);
+
+    Query<Employee> query = session.createQuery("from Employee", Employee.class);
+    return query.getResultList();
+  }
+
+//  public void saveEmployee(Employee employee) {
+//    Session session = sessionFactory.getCurrentSession();
+//    session.saveOrUpdate(employee);
+//  }
+//
+//  public Employee getEmployee(int id) {
+//    Session session = sessionFactory.getCurrentSession();
+//    return session.get(Employee.class, id);
+//  }
+//
+//  public void deleteEmployee(int id) {
+//    Session session = sessionFactory.getCurrentSession();
+//    Query<Employee> query = session.createQuery("delete from Employee where id =:employeeId");
+//    query.setParameter("employeeId", id);
+//    query.executeUpdate();
+//  }
+}
