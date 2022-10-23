e764837540c7cb389b0aea5b46e214706bc6daee
VladimirSafronov <safron5000@mail.ru>
2022-10-23 16:15:28 +0300

    created rest api chapter2
    * added dependency devtools

--- src/main/java/com/zaurtregulov/spring/springboot/springboot_rest/dao/EmployeeDAO.java
+++ src/main/java/com/zaurtregulov/spring/springboot/springboot_rest/dao/EmployeeDAO.java
@@ -7,9 +7,9 @@ public interface EmployeeDAO {
 
   List<Employee> getAllEmployees();
 
-//  void saveEmployee(Employee employee);
-//
-//  Employee getEmployee(int id);
-//
-//  void deleteEmployee(int id);
+  void saveEmployee(Employee employee);
+
+  Employee getEmployee(int id);
+
+  void deleteEmployee(int id);
 }
